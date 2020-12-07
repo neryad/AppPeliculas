dab499d5b21a101ec1409f0f29de6e70aa8f3e2f
neryad <dayern.gomez@hotmail.com>
2020-12-06 23:54:24 -0400

    fhdfgh

--- back-end/Controllers/WeatherForecastController.cs
+++ back-end/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace back_end.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            // sdf
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
